d78af410e125a5beaa485e137f572f47d98d4639
neswill1 <nenadkonatar@live.ac.me>
2015-03-17 13:54:41 +0100

    First commit

---
+++ first/settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="Target Center, Minneapolis"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Minneapolis";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=18778572070.;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
